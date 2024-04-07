c82434079e4bea2a2d2b64e1580f9e1e329b3bb6
Subhash Pandey <subhash@Subhashs-MacBook-Pro.local>
2024-04-07 20:06:10 +0530

    Minor changes looks like there is no git ignore file

--- obj/Debug/net8.0/test-website.AssemblyInfo.cs
+++ obj/Debug/net8.0/test-website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test-website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0c42127f55c7339ace706def81a4a9d15f57fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("test-website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test-website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
